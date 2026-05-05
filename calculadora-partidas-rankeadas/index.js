fa5d33f37bca221efafcd83566feb32ffaaef54a
alinedemoura1 <alinedemoura1@gmail.com>
2026-05-05 08:48:49 -0300

    commit 050526

--- calculadora-partidas-rankeadas/index.js
+++ calculadora-partidas-rankeadas/index.js
@@ -4,17 +4,17 @@ function calcRank(wins, losses) {
     let level = ""; // Variável para armazenar o nível do herói, que será decidido na estrutura de decisão abaixo
 
     // Estrutura que define o nível do herói com base no número de vitórias
-    if (wins <= 10) {
+    if (winBalance <= 10) {
         level = "Ferro";
-    } else if (wins >= 11 && wins <= 20) {
+    } else if (winBalance >= 11 && winBalance <= 20) {
         level = "Bronze";
-    } else if (wins >= 21 && wins <= 50) {
+    } else if (winBalance >= 21 && winBalance <= 50) {
         level = "Prata";
-    } else if (wins >= 51 && wins <= 80) {
+    } else if (winBalance >= 51 && winBalance <= 80) {
         level = "Ouro";
-    } else if (wins >= 81 && wins <= 90) {
+    } else if (winBalance >= 81 && winBalance <= 90) {
         level = "Diamante";
-    } else if (wins >= 91 && wins <= 100) {
+    } else if (winBalance >= 91 && winBalance <= 100) {
         level = "Lendário";
     } else {
         level = "Imortal";
